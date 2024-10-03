acaff4a87057ae60024ffedd7ccd296fc5c5d143
Neil Raina <mrneilraina@gmail.com>
2024-10-03 17:45:20 -0400

    use env variables

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import { RetoolRPC } from "retoolrpc"
 
 const rpc = new RetoolRPC({
-  apiToken: 'your-api-token-here',
-  host: 'https://portaltechnologies.retool.com',
-  resourceId: 'a29261fb-c099-4bc6-8c24-9e0b9dfb884d',
+  apiToken: process.env.API_TOKEN,
+  host: process.env.HOST,
+  resourceId: process.env.RESOURCE_ID,
   environmentName: 'production',
   pollingIntervalMs: 1000,
   version: '0.0.1', // optional version number for functions schemas
